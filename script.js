be4c6369b430558ab56d5a4fd10d16552d4c0916
Odehabuzaid <abuzaid163@hotmail.com>
2021-05-30 15:29:20 +0300

    Lab 5 - Loops

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let input 
-let tries = 0 
+let aNumber 
 
-do 
-{
-alert('To Enter this page , you must answer a question.')
-tries++
-input = prompt('What year did javascript start ?')
-
-}
-while ( input != '1995'  )
+    do{
+        alert('To Enter this page , you must answer a question.')
+        input = prompt('What year did javascript start ?')}
+        while ( input != '1995'  )
 
 
  
- 
+ function addRepeatedPicture(){
+     input = prompt('Rate this page . 1 to 10')
+         for (let i = 0 ; i < input ; i++){
+        document.write("<h1><img src='res/aStar.png' width='5%' hight='5%'><h1>")
+            }
+ }
 
 
 

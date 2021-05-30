53f639f6755ed7e304e10e0fa3a87817a91ede52
Odehabuzaid <abuzaid163@hotmail.com>
2021-05-30 14:18:30 +0300

    Lab 5 Loops

---
+++ script.js
@@ -0,0 +1,19 @@
+let input 
+let tries = 0 
+
+do 
+{
+alert('To Enter this page , you must answer a question.')
+tries++
+input = prompt('What year did javascript start ?')
+
+}
+while ( input != '1995'  )
+
+
+ 
+ 
+
+
+
+

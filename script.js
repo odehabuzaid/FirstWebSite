00fe96f7bf1fdaf68fb2918f49f6f012b98b85c0
Odehabuzaid <abuzaid163@hotmail.com>
2021-05-31 15:50:48 +0300

    Reading  Design web pages with CSS

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-let input 
-let aNumber 
+// let input 
+// let aNumber 
+//     do{
+//         alert('To Enter this page , you must answer a question.')
+//         input = prompt(`What year did javascript start ?   '1996'`)}
+//         while ( input != '1996'  )
 
+// //  function addRepeatedPicture(){
+// //      input = prompt('Rate this page . 1 to 5 ?')
+// //          if (input >= 0 && input <= 5 ){
+// //             for (let i = 0 ; i < input ; i++){
+// //                 document.write(`<img src='res/aStar.png' width='5%' hight='5%'>`) }
 
-    do{
-        alert('To Enter this page , you must answer a question.')
-        input = prompt("What year did javascript start ?   '1996'")}
-        while ( input != '1996'  )
-
-   
-
-
-
-    document.addEventListener("ContentLoaded", addRepeatedPicture())
-
- function addRepeatedPicture(){
-     input = prompt('Rate this page . 1 to 5 ?')
-         if (input >= 0 && input <= 5 ){
-            for (let i = 0 ; i < input ; i++){
-                document.write("<img src='res/aStar.png' width='5%' hight='5%'>")
-                    }
-
-       document.write('<h1>' + input + ' Out of 5</h1>')
-
-         }else {addRepeatedPicture()}
-
- }
-
+// //        document.write(`<h1>  ${input} out of 5</h1>`)
+// //          }else {addRepeatedPicture()}
+// //  }
+// //  document.addEventListener('ContentLoaded', addRepeatedPicture())
 
 
 
